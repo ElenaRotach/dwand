299e0fc44ff0fc5fce35fc1cd9fbf020bceabd26
ElenaRotach <rotach.elena@gmail.com>
2017-08-21 15:01:53 +0300

    main page style

--- views/post/post.php
+++ views/post/post.php
@@ -1,46 +1,32 @@
 <?php
-
+use assets\PostAsset;
 $this->title = $model->title;
 ?>
-<script>
-    function goComment(obj, post_id, user_id) {
-        var text = $(obj).parent().children('input').val();
-        if(text!="") {
-            $.ajax({
-                url: '/admin/comment/create',
-                type: 'POST',
-                data: {
-                    'post_id': post_id,
-                    'user_id': user_id,
-                    'test': text
-                },
-                success: function (data) {
-                    //переадрессация
-                    //console.log(data);
-                },
-                error: function () {
-                    console.log('Внутренняя ошибка сервера');
-                }
-            });
-        }else{
-            return false;
-        }
-    }
-</script>
-<div class="site-index">
-    <H1><?= $model->title ?></H1>
+<div class="post_detail light-primary-color">
+    <H1 class ="primary-text-color"><?= $model->title ?></H1>
     
     <span><?= $model->description ?></span>
 </div>
-<div id="add_new_comment">
-    <input type="text" id="text_comment">
-    <a class="btn btn-default" onclick="goComment(this, <?= $model->id ?>, <?= Yii::$app->user->id ?>)">Отправить</a>
+
+<div id="add_new_comment" class="col-lg-12">
+    <h4>Введите Ваш комментарий</h4>
+    <textarea id="text_comment" class="col-lg-12 col-md-12 col-xs-12"></textarea>
+    <a class="go_btn" onclick="goComment(this, <?= $model->id ?>, <?= Yii::$app->user->id ?>)">Отправить</a>
 </div>
-<div>
+
+<div class="comments_blok col-lg-12 col-xs-12">
     <?php if(count($comments)>0):?>
         <?php foreach ($comments as $item):?>
-            <? var_dump(count($comments)); exit();?>
             <div><?=""?></div>
+            <div class="comment dark-primary-color">
+                <div class="author">
+                    <div class="avatar"><img class="avatar_img" src="./img/1.jpg"></div>
+                    <div class="author_name text-primary-color">Дэн Симмонс</div>
+                </div>
+                <div class="comment_text light-primary-color">
+                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque iaculis mi lacus, et porttitor metus vehicula id. Nullam vitae augue lobortis, consequat quam ac, dapibus eros. Nunc eleifend, lorem placerat cursus ultricies, tellus libero varius ante, eget porta eros mauris ut mi. Suspendisse condimentum egestas erat, consequat tempus dolor vehicula et. Aenean ut neque in risus condimentum semper in nec nibh. Curabitur lorem felis, dignissim sed tincidunt et, vehicula non felis. Suspendisse faucibus placerat egestas. Cras consequat viverra mauris, ut lacinia mi facilisis a. Quisque iaculis orci eu tincidunt mollis. Morbi et volutpat felis. Aliquam lacinia congue nibh, eu ultricies lectus consequat ac. Quisque sed auctor orci, a venenatis mauris. Praesent nec convallis orci, facilisis ultrices nisl. Sed condimentum vehicula consequat.</span>
+                </div>
+            </div>
         <?php endforeach ?>
     <?php endif;?>
 </div>
--- views/site/index.php
+++ views/site/index.php
@@ -1,6 +1,7 @@
 <?php
 use yii\helpers\Html;
 use app\models\Comment;
+use assets\MainAsset;
 /* @var $this yii\web\View */
 
 $this->title = 'task';
@@ -9,16 +10,19 @@ $this->title = 'task';
 
     <?php if(count($model)): ?>
         <?php foreach ($model as $item): ?>
-            <? var_dump($comments);exit(); ?>
+            <? //var_dump($comments);exit(); ?>
             <? //var_dump($comments);exit(); ?>
             <article class="col-lg-12">
-                <h3><?= Html::a($item->title, ['/post/post', 'id' => $item['id']]) . '&emsp;<i class="fa fa-external-link"></i>' ?></h3>
-                <span><?= substr($item->description, 0, 500) ?>
-                    <?= Html::a("...", ['/post/post', 'id' => $item['id']]) . '&emsp;<i class="fa fa-external-link"></i>' ?>
-                </span>
-                <div class="col-lg-6">Просмотров: <?= $views = $item->views ? :'0' ?></div>
-                <div class="col-lg-6">Комментарии: <?= $comments[$item->id] ?></div>
+                <div class="post_content">
+                    <h3><?= Html::a($item->title, ['/post/post', 'id' => $item['id']]) . '&emsp;<i class="fa fa-external-link"></i>' ?></h3>
+                    <span><?= substr($item->description, 0, 500) ?>
+                        <?= Html::a("...", ['/post/post', 'id' => $item['id']]) . '&emsp;<i class="fa fa-external-link"></i>' ?>
+                    </span>
+                </div>
+                <div class="views col-lg-6"><i class="fa fa-eye" aria-hidden="true"></i>&emsp;Просмотров: <?= $views = $item->views ? :'0' ?></div>
+                <div class="comments col-lg-6"><i class="fa fa-comments-o" aria-hidden="true">Комментарии: <?= $comments[$item->id] ? :'0' ?></div>
             </article>
+            <hr>
         <?php endforeach ?>
     <?php endif; ?>
 </div>
